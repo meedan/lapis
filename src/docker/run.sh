7ab6425d5a3df7dd0d20cc69f5a9586f3e4cb60d
Benjamin Foote <git@bnf.net>
2016-06-15 11:23:04 -0700

    move Dockerfile to src directory v1.1-ruby2.3.0-nginx1.8.1

--- src/docker/run.sh
+++ src/docker/run.sh
@@ -2,15 +2,14 @@
 
 dir=$(pwd)
 cd $(dirname "${BASH_SOURCE[0]}")
-cp Dockerfile ..
 cd ..
 
 # Build
-docker build -t lapis/%app_name% .
+docker build -t meedan/%app_name% .
 
 # Run
 secret=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-docker run -d -p 3000:80 --name %app_name% -e SECRET_KEY_BASE=$secret lapis/%app_name%
+docker run -d -p 3000:80 --name %app_name% -e SECRET_KEY_BASE=$secret meedan/%app_name%
 
 echo
 docker ps | grep '%app_name%'
@@ -20,5 +19,3 @@ echo '-----------------------------------------------------------'
 echo 'Now go to your browser and access http://localhost:3000/api'
 echo '-----------------------------------------------------------'
 
-rm Dockerfile
-cd $dir
